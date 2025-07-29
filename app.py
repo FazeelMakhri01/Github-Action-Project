cad3c635929ac66e2a6bf076f79055efa599e514
FazeelMakhri01 <fazeelullah@gmail.com>
2025-07-29 17:14:30 +0100

    Final Version

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import sys
 import requests
 
+
 def get_weather(city):
+
     try:
         url = f"https://wttr.in/{city}?format=3"
         response = requests.get(url)
         print(response.text)
     except Exception as e:
         print("Error fetching weather data:", e)
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
